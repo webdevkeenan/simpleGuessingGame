1556eda7b56eabd5b31982407dc8f8afb2bc434a
Keenan <42125735+Styleshift@users.noreply.github.com>
2023-03-05 15:32:39 -0500

    debug

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let maximum = parseInt(prompt("Enter the maximum number! (Type \"q\" or \"quit\" to end game)"));
+let maximum = parseInt(prompt("Enter the maximum number!"));
 while (!maximum) {
     maximum = parseInt(prompt("Enter a valid number!"));
 }
@@ -12,10 +12,10 @@ let attempts = 1;
 while(parseInt (guess) !== targetNum) {
     if (guess === 'q') break;
     attempts++;
-    if(guess > targetNum) {
-        guess = (prompt("Too High Enter Another Guess OR (Type \"q\" to end game)"));
+    if (guess > targetNum) {
+        guess = prompt("Too High Enter Another Guess OR (Type \"q\" to end game)");
     } else {
-        guess = (prompt("Too Low! Enter A New Guess OR (Type \"q\" or \"quit\" to end game)"));
+        guess = prompt("Too Low! Enter A New Guess OR (Type \"q\" or \"quit\" to end game)");
     }
 }
 if (guess === 'q') {
